bdb1d58e99b6c437537372b3e9144dc89a21ed4d
Justin Shields <jshields@developod.com>
2018-02-23 14:43:42 -0800

    Refactored NameNode.py

--- Namenode/NameNode.py
+++ Namenode/NameNode.py
@@ -6,29 +6,23 @@ import os
 from threading import Thread, Lock
 
 class Namenode:
-    '''
+    """
     Namenode needs to do two things
     1) Which blocks are part of which files
     2) Picking N different Datanodes to store each block (then return list of blocks and datanodes)
     3) only one writer at a time so we need a lock and thread for writeFile
-    '''
-
-    #Dictionary for which blocks are part of which file
-    fileD = {}
-    #Dictionary for which datanodes are storing each block
-    blockD = {}
-
-    mutex = Lock()
-
+    """
     def __init__(self):
-        self.REPLICATION = 3 #pick 3 different datanodes to store each block by default
+        self.REPLICATION = 3 # pick 3 different datanodes to store each block by default
+        self.fileD = {} # Dictionary for which blocks are part of which file
+        self.blockD = {} # Dictionary for which datanodes are storing each block
+        self.mutex = Lock()
 
-
-    def writeFile(filename, blocks):  #pass in array of blocks as arguments
+    def writeFile(self, filename, blocks):  #pass in array of blocks as arguments
 
         uniqueFile = filename
         #need to find out how to make 'uniqueFile' the name of the file otherwise dictionary overwrites itself everytime method is called
-        fileD['uniqueFile'] = blocks
+        self.fileD['uniqueFile'] = blocks
 
         '''
         For each block from file, we need to apply replication factor 
@@ -39,9 +33,14 @@ class Namenode:
 
         #return list of blocks and datanodes back to client
 
-
-    #The block report given from the data node
-    def blockReport(datanodeNum, blocks ): #pass in all blocks as array assigned to the specific datanodeNumber e.g. datanode1,datanode2,etc
+    def blockReport(self, datanodeNum, blocks ):
+        """
+        The block report given from the data node
+        Pass in all blocks as array assigned to the specific datanodeNumber e.g. datanode1,datanode2,etc
+        :param datanodeNum:
+        :param blocks:
+        :return:
+        """
 
 
 
