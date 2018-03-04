df8c24afd728d1669d11c2628a158a388ce78cfc
HangNguyen <nguye404@seattleu.edu>
2018-03-03 17:04:58 -0800

    dataNode.py and dataNodeServer.py

--- Datanode/DataNode.py
+++ Datanode/DataNode.py
@@ -9,14 +9,27 @@ class DataNode:
 
     # server to another DataNode
     def giveBlock(self, blockID, DataNodeID):
+        otherdn = xmlrpclib.ServerProxy(DataNodeID)
+        # string in the current blockID
+        blockData = ""
+        otherdn.receiveBlock(blockID, blockData)
+
 
     # client to another DataNode
-    def receiveBlock(self, block):
+    def receiveBlock(self, blockID, blockData):
+        # save new block & update report
+        # 1) create a file
+        # 2) write blockData to that file
+        # 3) close the file
+        # 4) update BlockReport (slef.listBlockID)
+        # 5) send BlockReport to NameNode
+
 
     # server to NameNode
     def sendBlockReport(self, NameNodeID):
         # receiveBlockReport ~ whatever the name of the func in NameNode
-        return self.namenode.receiveBlockReport(self.ip, listBlockID)
+        return self.namenode.receiveBlockReport(self.ip, self.listBlockID)
+
 
     def sendHeartBeat(self):
 
