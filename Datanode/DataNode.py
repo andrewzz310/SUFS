80d85bef1e1c2c34c8424e8664134c90fa49c612
HangNguyen <nguye404@seattleu.edu>
2018-03-06 11:18:56 -0800

    delete path in the parent directory and ls

--- Datanode/DataNode.py
+++ Datanode/DataNode.py
@@ -1,35 +1,26 @@
 import xmlrpclib
-from modules import nnRPCClient
 
 
 class DataNode:
-    def __init__(self, ip, nnIp, nnPort):
+    def __init__(self):
         self.listBlockID = []
-        self.ip = ip
-        self.nnRPC = nnRPCClient.nnRPCClient(nnIp, nnPort)
+        self.ip = ""
+        self.namenode = xmlrpclib.ServerProxy(self.ip)
 
     # server to another DataNode
-    # probably won't be used
-    # definitely don't use this
     def giveBlock(self, blockID, DataNodeID):
         otherdn = xmlrpclib.ServerProxy(DataNodeID)
         # string in the current blockID
         blockData = ""
         otherdn.receiveBlock(blockID, blockData)
-
+        return ""
 
     # client to another DataNode
-    # probably won't be used
-    # definitely don't use this
     def receiveBlock(self, blockID, blockData):
-        return ""
         # save new block & update report
         # 1) create a file
         # 2) write blockData to that file
-        # 3) close the file
-        # 4) update BlockReport (slef.listBlockID)
-        # 5) send BlockReport to NameNode
-
+        return ""
 
     # server to NameNode
     def sendBlockReport(self, NameNodeID):
@@ -37,6 +28,8 @@ class DataNode:
         return self.namenode.receiveBlockReport(self.ip, self.listBlockID)
 
 
+
+
     def sendHeartBeat(self):
-        return ""
+
 
