1ceb78da14ef921d7b6345cdf66e6c0a077b00da
Alex Lew <alew104@gmail.com>
2018-03-13 13:01:55 -0700

    fix replication I think

--- Namenode/NamenodeServer.py
+++ Namenode/NamenodeServer.py
@@ -92,10 +92,10 @@ def replicate(curRepFac, block):
     while (rep < nn.REPLICATION and counter < len(nn.alive)):
         for targetip in nn.alive.keys():
             print (targetip)
-            if (targetip not in nn.blockD.get(block)):
+            if (targetip not in nn.blockD.get(block, [targetip])):
                 blocksrc.targetBlock(block, targetip)
                 rep += 1
-                print (block + " rep factor: " + rep)
+                print (block + " rep factor: " + str(rep))
             counter += 1
 
 
